e0158cf76d780426ea6183ca59a3a16f28899647
Walter Aab <aabwalter@yahoo.com>
2021-08-01 19:45:05 -0500

    Add HarvestSiteSerializer to HarvestSite controller

--- app/controllers/harvest_sites_controller.rb
+++ app/controllers/harvest_sites_controller.rb
@@ -3,45 +3,46 @@ class HarvestSitesController < ApplicationController
 
   # GET /harvest_sites
   def index
-    @harvest_sites = HarvestSite.all
+    harvest_sites = HarvestSite.all
 
-    render json: @harvest_sites
+    render json: HarvestSiteSerializer.new(harvest_sites)
   end
 
   # GET /harvest_sites/1
   def show
-    render json: @harvest_site
+    render json: HarvestSiteSerializer.new(harvest_site)
   end
 
   # POST /harvest_sites
   def create
-    @harvest_site = HarvestSite.new(harvest_site_params)
+    harvest_site = HarvestSite.new(harvest_site_params)
 
-    if @harvest_site.save
-      render json: @harvest_site, status: :created, location: @harvest_site
+    if harvest_site.save
+      #render json: harvest_site, status: :created, location: harvest_site
+      render json: HarvestSiteSerializer.new(harvest_site)
     else
-      render json: @harvest_site.errors, status: :unprocessable_entity
+      render json: harvest_site.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /harvest_sites/1
   def update
-    if @harvest_site.update(harvest_site_params)
-      render json: @harvest_site
+    if harvest_site.update(harvest_site_params)
+      render json: HarvestSiteSerializer.new(harvest_site)
     else
-      render json: @harvest_site.errors, status: :unprocessable_entity
+      render json: harvest_site.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /harvest_sites/1
   def destroy
-    @harvest_site.destroy
+    harvest_site.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_harvest_site
-      @harvest_site = HarvestSite.find(params[:id])
+      harvest_site = HarvestSite.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
