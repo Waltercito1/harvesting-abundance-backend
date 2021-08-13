f80cad70e5e851df7b1c5715697650a55062af58
Walter Aab <aabwalter@yahoo.com>
2021-08-12 20:28:24 -0500

    Set up api for location nested form

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,10 @@
 class Location < ApplicationRecord
+    validates :latitude, presence: true
+    validates :longitude, presence: true
+
     has_many :harvest_sites
     has_many :trees, through: :harvest_sites
+
+    accepts_nested_attributes_for :trees
+    accepts_nested_attributes_for :harvest_sites
 end
