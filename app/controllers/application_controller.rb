d1b9c8c2515dd4a7073fb13967e1b6b68b0b4ce5
Walter Aab <aabwalter@yahoo.com>
2021-08-20 13:20:11 -0500

    wip fix bug on user create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::API
     include ActionController::Cookies
     protected
         def configure_permitted_parameters
-            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :remember_me) }
+            # byebug
+            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :remember_me, :first_name, :last_name) }
             devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
             devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password) }
         end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    # byebug
     user = User.new(user_params)
 
     if user.save
@@ -38,6 +39,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:username, :email, :password_digest)
+      params.require(:user).permit(:first_name, :last_name, :username, :email, :password_digest)
     end
 end
