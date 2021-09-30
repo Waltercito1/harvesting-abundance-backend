c511213f0ad5a725b7850791871f04914b4f18a7
Walter Aab <aabwalter@yahoo.com>
2021-09-29 19:48:47 -0500

    Remove unused Devise parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,5 @@ class ApplicationController < ActionController::API
     protected
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :remember_me])
-            # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :remember_me, :first_name, :last_name) }
-            # devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
-            # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password) }
         end
 end
